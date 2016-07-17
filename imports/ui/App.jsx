8af88633e1bb832230ed0a5e94d01507d9fb7b3f
Spyros Psychogios <spyros@enki.com>
2016-07-17 12:31:47 +0100

    first push

---
+++ imports/ui/App.jsx
@@ -0,0 +1,90 @@
+import React, {PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+import {createContainer} from 'meteor/react-meteor-data';
+
+import {Jobs} from '../api/jobs';
+import Job from './Job.jsx';
+
+const App = React.createClass({
+  PropTypes: {
+    jobs: PropTypes.array,
+  },
+
+  getInitialState() {
+    return {
+      submitActive: false,
+    };
+  },
+
+  getJobs() {
+    return [];
+  },
+
+  renderJobs() {
+    return (<div className="jobs-container">
+      {this.props.jobs.map((job, i) => {
+        return (<Job job={job} key={i} />);
+      })}
+    </div>);
+  },
+
+  render() {
+    const {submitActive} = this.state;
+    return (
+      <div>
+        <div className="header">
+          <h1>GoQuoteMe</h1>
+        </div>
+        <div className="container">
+          <div className="job-create">
+            <h2>Create a job:</h2>
+            <form className="new-job"
+              onSubmit={this.handleCreateJob}>
+              <input type="text" ref="jobTitle"
+                placeholder="Name your job"
+                onChange={this.handleTitleChange} />
+              <textarea type="text" ref="jobDesc" className="desc"
+                placeholder="Describe your job" rows="8"/>
+              <button type="submit" value="Submit"
+                className={'submit ' + (submitActive ? 'active' : 'disabled')}>
+                Submit
+              </button>
+            </form>
+          </div>
+          {this.renderJobs()}
+        </div>
+      </div>
+    );
+  },
+
+  handleCreateJob(e) {
+    e.preventDefault();
+    const job = {
+      title: e.target[0].value,
+      description: e.target[1].value,
+      bids: [],
+    };
+    if (!job.title || job.title.length < 3) {
+      return;
+    }
+    Jobs.insert(job);
+
+    this.refs.jobTitle.value = '';
+    this.refs.jobDesc.value = '';
+  },
+
+  handleTitleChange(e) {
+    e.preventDefault();
+    if (e.target.value && e.target.value.length >= 3) {
+      this.setState({submitActive: true});
+    } else {
+      this.setState({submitActive: false});
+    }
+  }
+});
+
+export default createContainer(() => {
+  return {
+    jobs: Jobs.find().fetch()
+  }
+}, App);
---
+++ imports/ui/Job.jsx
@@ -0,0 +1,50 @@
+import React, {PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+// import {createContainer} from 'meteor/react-meteor-data';
+
+import {Jobs} from '../api/jobs';
+
+const Job = React.createClass({
+  PropTypes: {
+    job: PropTypes.object,
+  },
+
+  render() {
+    const {job} = this.props;
+    return (<div className="job">
+      <h2>{job.title}</h2>
+      <p className="job-desc">
+        {job.description}
+      </p>
+      <ul className="job-bids">
+        {job.bids.map((bid, j) => {
+          return <li key={j}>
+            <strong>{bid.description}</strong> {bid.price}£
+          </li>
+        })}
+      </ul>
+      <form key={'form'} className="new-bid" onSubmit={this.handleBid}>
+        <input type="text" ref="bidDesc"
+          placeholder="Type info about your bid..." />
+        <input type="number" ref="bidPrice"
+          placeholder="Price" />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>);
+  },
+
+  handleBid(e) {
+    e.preventDefault();
+
+    const bid = {
+      description: e.target[0].value,
+      price: e.target[1].value,
+      createdAt: new Date(),
+    }
+    Jobs.update(this.props.job._id, {
+      $push: {bids: bid},
+    });
+  },
+});
+
+export default Job;
